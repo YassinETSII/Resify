e007144eb805ef2552dc0e09adb8c0d97354200b
Alfonso Rodriguez <2aalfonsoromero@gmail.com>
2020-03-16 22:29:27 +0100

    Arreglada asociación Residencia - Manager

--- src/main/java/org/springframework/samples/petclinic/model/Residencia.java
+++ src/main/java/org/springframework/samples/petclinic/model/Residencia.java
@@ -21,7 +21,7 @@ import java.time.LocalTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -117,7 +117,7 @@ public class Residencia extends BaseEntity {
 	}
 
 
-	@ManyToOne
+	@OneToOne
 	@JoinColumn(name = "manager_id")
 	private Manager manager;
 
