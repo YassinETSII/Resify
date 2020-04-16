02134a038cfdc2aeab3a67832eb4e2733cf10ba3
Alfonso Rodriguez <2aalfonsoromero@gmail.com>
2020-04-16 14:59:34 +0200

    UI Tests para HUs #2, #3 y #4

---
+++ src/test/java/org/springframework/samples/petclinic/interfaz/HU3NegativoUITest.java
@@ -0,0 +1,105 @@
+
+package org.springframework.samples.petclinic.interfaz;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HU3NegativoUITest {
+
+	@LocalServerPort
+	private int				port;
+
+	private WebDriver		driver;
+	private String			baseUrl;
+	private boolean			acceptNextAlert		= true;
+	private StringBuffer	verificationErrors	= new StringBuffer();
+
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		this.driver = new FirefoxDriver();
+		this.baseUrl = "https://www.google.com/";
+		this.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	//El manager accede a las inscripciones pendientes
+	//y elige la de un anciano, entrando en la edición de la misma.
+	//El manager decide aceptar la inscripción, pero el sistema le informa
+	//de que el anciano no cumple las condiciones para entrar en la residencia
+	//y le devuelve el formulario
+	@Test
+	public void testHU3NegativoUI() throws Exception {
+		this.driver.get("http://localhost:" + this.port);
+		this.driver.findElement(By.xpath("//a[contains(@href, '/login')]")).click();
+		this.driver.findElement(By.id("username")).clear();
+		this.driver.findElement(By.id("username")).sendKeys("manager1");
+		this.driver.findElement(By.id("password")).clear();
+		this.driver.findElement(By.id("password")).sendKeys("manager1");
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+		this.driver.findElement(By.xpath("//a[contains(@href, '/inscripciones')]")).click();
+		this.driver.findElement(By.linkText("Dolores Prieto")).click();
+		this.driver.findElement(By.linkText("Editar inscripción")).click();
+		new Select(this.driver.findElement(By.id("estado"))).selectByVisibleText("aceptada");
+		this.driver.findElement(By.xpath("//option[@value='aceptada']")).click();
+		this.driver.findElement(By.xpath("//button[@type='submit']")).click();
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		this.driver.quit();
+		String verificationErrorString = this.verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			Assert.fail(verificationErrorString);
+		}
+	}
+
+	private boolean isElementPresent(final By by) {
+		try {
+			this.driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			this.driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = this.driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (this.acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			this.acceptNextAlert = true;
+		}
+	}
+}
